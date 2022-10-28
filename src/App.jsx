14e04664c26941fee5bd8062f9fd98099d40ded4
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-10-28 13:14:16 -0700

    Chaning Daily Count

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import UCR from './assets/ucr.jpg'
 import './App.css'
 
 function App() {
-  var [DocsCount, setDocsCount] = useState(211)
+  var [DocsCount, setDocsCount] = useState(252)
 
   // const countingDocs = async () => {
   //   const coll = collection(db, "signed");
