cd89a44b302101219f303898aa5495feb763e59c
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-11-13 18:10:40 -0800

    Updating Count

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import UCR from './assets/ucr.jpg'
 import './App.css'
 
 function App() {
-  var [DocsCount, setDocsCount] = useState(440)
+  var [DocsCount, setDocsCount] = useState(454)
 
   // const countingDocs = async () => {
   //   const coll = collection(db, "signed");
